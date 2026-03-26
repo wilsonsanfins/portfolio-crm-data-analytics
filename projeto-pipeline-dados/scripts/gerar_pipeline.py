411df55cf745cd1578f58e664ce6b8546d4e878a
wilsonsanfins <162934920+wilsonsanfins@users.noreply.github.com>
2026-03-25 23:21:30 -0300

    Update gerar_pipeline.py

--- projeto-pipeline-dados/scripts/gerar_pipeline.py
+++ projeto-pipeline-dados/scripts/gerar_pipeline.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-# Simulando as 3 bases 
+# Simulando as 3 "bases"
 clientes = pd.DataFrame({'id_cliente': [1, 2], 'nome': ['Joao', 'Maria']})
 interacoes = pd.DataFrame({'id_cliente': [1, 1, 2], 'campanha': ['Black Friday', 'Natal', 'Black Friday'], 'abriu_email': [1, 0, 1]})
 
-# O "Merge" (União dos dados)
+# União dos dados
 df_final = pd.merge(interacoes, clientes, on="id_cliente", how="left")
 
 # Salvando o resultado
