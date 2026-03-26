3c52024cf8b292c2043cf9365b5a158478ad86bb
wilsonsanfins <162934920+wilsonsanfins@users.noreply.github.com>
2026-03-25 23:03:23 -0300

    Update processar_dados.py

--- projeto-crm-dados/scripts/processar_dados.py
+++ projeto-crm-dados/scripts/processar_dados.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# 1. Simulando a criação de dados 
+# Simula a criação de dados 
 dados = {
     'id_cliente': [1, 2, 3, 4],
     'nome': ['Joao', 'Maria', 'Jose', 'Ana'],
@@ -10,11 +10,10 @@ dados = {
 
 df = pd.DataFrame(dados)
 
-# 2. Criando a regra de segmentação
-# Se comprou há mais de 90 dias, é "Inativo". Se gastou mais de 2000, é "VIP".
+# Função que se comprou há mais de 90 dias, é "Inativo". Se gastou mais de 2000, é "VIP"
 df['status'] = df['dias_sem_comprar'].apply(lambda x: 'Ativo' if x <= 90 else 'Churn/Inativo')
 df['segmento'] = df['valor_compra'].apply(lambda x: 'Premium/VIP' if x >= 2000 else 'Standard')
 
-# 3. Salvando o resultado na pasta data
+# Salvando o resultado na pasta data
 df.to_csv('data/clientes_final.csv', index=False)
 print("Arquivo processado com sucesso na pasta data!")
